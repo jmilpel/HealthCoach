8c276a43f096063a262a3a7f3ab9dd00599a9a72
jmilpel <jmilpel@gmail.com>
2018-05-22 18:04:12 +0200

    180522_02

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import { GoogleMaps } from 'meteor/dburles:google-maps';
+import { Tracker } from 'meteor/tracker';
 
 Meteor.startup(() => {
   // code to run on server at startup
