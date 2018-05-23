82a73e9de700aca30279023e309dbfa03f701885
jmilpel <jmilpel@gmail.com>
2018-05-23 15:04:40 +0200

    Configuración del blog

--- client/js/main.js
+++ client/js/main.js
@@ -15,7 +15,7 @@ GoogleMaps.load();
 
 
 Template.Contact.helpers({
-    exampleMapOptions: function() {
+    healthMapOptions: function() {
       // Make sure the maps API has loaded
       if (GoogleMaps.loaded()) {
         // Map initialization options
@@ -29,7 +29,7 @@ Template.Contact.helpers({
 
 Template.Contact.onCreated(function() {
     // We can use the `ready` callback to interact with the map API once the map is ready.
-    GoogleMaps.ready('exampleMap', function(map) {
+    GoogleMaps.ready('healthMap', function(map) {
       // Add a marker to the map once it's ready
       var marker = new google.maps.Marker({
         position: map.options.center,
@@ -39,30 +39,27 @@ Template.Contact.onCreated(function() {
   });
 
 
-Books = new Mongo.Collection("books");
-Books.attachSchema(new SimpleSchema({
-  name: {
-    type: String,
-    label: "Nombre y apellidos",
-    max: 200
-  },
-  email: {
-    type: String,
-    label: "Email"
-  },
-  telephone: {
-    type: Number,
-    label: "Teléfono",
-    min: 0
-  },
-  city: {
-    type: String,
-    label: "Población",
-    optional: true
-  },
-  query: {
-    type: String,
-    label: "Dudas y consultas",
-    max: 1000
+Schema = {};
+Schema.contactFormSchema = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Your name",
+        max: 50
+    },
+    email: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email,
+        label: "E-mail address"
+    },
+    message: {
+        type: String,
+        label: "Message",
+        max: 1000
+    }
+}, { tracker: Tracker });
+
+Template.contactForm.helpers({
+  contactFormSchema: function() {
+    return Schema.contactFormSchema;
   }
-}, { tracker: Tracker }));
+});
--- server/main.js
+++ server/main.js
@@ -6,3 +6,26 @@ Meteor.startup(() => {
   // code to run on server at startup
 	//GoogleMaps.load();
 });
+
+
+Meteor.methods({
+  sendEmail: function(doc) {
+    // Important server-side check for security and data integrity
+    check(doc, Schema.contactFormSchema);
+
+    // Build the e-mail text
+    var text = "Name: " + doc.name + "\n\n"
+            + "Email: " + doc.email + "\n\n\n\n"
+            + doc.message;
+
+    this.unblock();
+
+    // Send the e-mail
+    Email.send({
+        to: "jmilpel@gmail.com",
+        from: doc.email,
+        subject: "Website Contact Form - Message From " + doc.name,
+        text: text
+    });
+  }
+});
